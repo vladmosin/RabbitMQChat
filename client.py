99803b95342ed8ff5af5b53be313fd31b02fc2eb
Vladislav Mosin <vmos1999@mail.ru>
2020-04-29 16:34:24 +0300

    part of functionality implemented

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import pika
+from threading import Thread
+
 
 class Chat:
     def __init__(self, name):
@@ -10,24 +12,37 @@ class Client:
     def __init__(self, name, channel):
         self.active_chat = None
         self.name = name
-        self.chats = []
+        self.chats = {}
         self.channel = channel
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+    def send_message(self, message):
+        pass
 
-def send_message(message):
-    pass
+    def switch_to_chat(self, chat_name):
+        self.channel.exchange_declare(chat_name)
+
+        if chat_name not in self.chats:
+            self.chats[chat_name] = Chat(chat_name)
+
+        self.active_chat = self.chats[chat_name]
 
 
-def switch_to_chat(chat_name):
-    pass
+    def read_message(self, message):
+        pass
 
 
-def read_message(message):
-    pass
+def get_name():
+    return "Vasya"
 
 
 if __name__ == "__main__":
-    pass
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    name = get_name()
+    client = Client(name=name, channel=channel)
+
+    consuming = Thread(client.channel.start_consuming)
+    consuming.start()
+    consuming.join()
